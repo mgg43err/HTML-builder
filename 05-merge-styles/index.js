9f88b10b520f042bcfb15291ebcf9b36e20d00ee
mgg43er <39748734+mgg43err@users.noreply.github.com>
2022-11-09 06:13:47 +0300

    fix: 05 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,39 @@
+const path = require("node:path");
+const fs = require("node:fs");
+
+const outputpath = path.join(__dirname + "/project-dist/bundle.css");
+const inputdir = path.join(__dirname + "/styles/");
+
+function bundleExistCheck(src) {
+	fs.access(src, err => {
+		if (err) {
+			console.error(err);
+		}
+	});
+	return true;
+}
+
+function bundleRiser(src, dest) {
+	if (bundleExistCheck(dest) === true) {
+		fs.rm(dest, {recursive: true, force: true}, () => {
+			const ws = fs.createWriteStream(dest);
+			fs.promises
+				.readdir(src, {withFileTypes: true})
+				.then(files => {
+					files.map(x => {
+						if (path.extname(x.name) === ".css") {
+							let rs = fs.createReadStream(path.join(src + x.name), "utf-8");
+							rs.on("data", data => ws.write(data + "\n"));
+						}
+					});
+				})
+				.catch(err => {
+					console.log(err);
+				});
+		});
+	}
+	const ws = fs.createWriteStream(dest);
+
+	return {src, dest};
+}
+bundleRiser(inputdir, outputpath);
