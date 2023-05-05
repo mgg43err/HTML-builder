104a88ab724529670ad72746438a0a31c469be2c
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2023-05-05 23:56:26 +0300

    fix: 02 exit word writing to file out.txt

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,29 +1,29 @@
-/*
-приветствие
-ожидающий ввод текста,
-записывающий введённый текст в файл.
-*/
 const fs = require("fs");
 const readline = require("readline");
 const path = require("path");
 const process = require("process");
-
 const ws = fs.createWriteStream(path.join(__dirname + "/out.txt"));
-
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
+const b = () => console.log(`Bye! My dear Friend`);
+
+
 console.log("Hello my dear Friend!");
+
+
 rl.on("line", line => {
-	ws.write(line + "\n");
-	if (line === "exit") {
-		console.log("Bye! My dear Friend");
+	rl.on("SIGINT", () => {
+		b();
+		process.exit(1);
+	});
+	if (line != "exit"){
+		ws.write(line + "\n");
+	}
+	else if (line === "exit") {
+		b()
 		process.exit(1);
 	}
-});
-
-rl.on("SIGINT", () => {
-	console.log("Bye! My dear Friend");
-	process.exit(1);
+	
 });
