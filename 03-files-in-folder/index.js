b01055c5503f72f4383b741fe01ecf53ca13008a
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2023-05-06 01:09:16 +0300

    feat: formatting

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,17 +2,17 @@ const path = require('path');
 const fs = require('fs');
 const SECRET_FOLDER = path.join(__dirname + '/secret-folder/');
 
-fs.promises.readdir(SECRET_FOLDER, {withFileTypes:true})
+fs.promises.readdir(SECRET_FOLDER, { withFileTypes: true })
     .then(filenames => {
         filenames.map(x => {
             x.isFile() ?
-                fs.stat(path.join(SECRET_FOLDER  +x.name ), (e, s) => {
+                fs.stat(path.join(SECRET_FOLDER + x.name), (e, s) => {
                     if (e) { console.log(e) }
                     else {
-                        console.log(x.name, "-", path.parse(SECRET_FOLDER  +x.name).ext, "-", (s.size/1024).toFixed(3) +"kb" );
+                        console.log(x.name, "-", path.parse(SECRET_FOLDER + x.name).ext, "-", (s.size / 1024).toFixed(3) + "kb");
                     }
                 }) :
-                "";                    
+                "";
         }
         )
     })
