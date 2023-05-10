df5790dbc09a6aa03a125c4720e39eb5c2e23000
ap <ap@debian-BULLSEYE-live-builder-AMD64>
2023-05-10 18:04:51 +0300

    fix: unused variables

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,9 +1,7 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const stdout = require("node:process");
 
 let txt = path.join(__dirname + "/text.txt");
-
 let readStream = fs.createReadStream(txt, "utf-8");
 
 readStream.on("data", data => console.log(data));
